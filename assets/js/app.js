0ef0f86dca3fe17bfd4e041eb07f93d044dd4c87
Tiam Taheri <taheri@sport-thieme.net>
2017-02-21 13:57:40 +0100

    preparing data & bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,12 @@ $(function () {
   console.log('ready');
   function getWeather(city, callback) {
     var url = 'http://api.openweathermap.org/data/2.5/weather';
+    var apiKey = '4846f8ae0e168b5a24906e0c47ff92b5';
     $.ajax({
       dataType: "jsonp",
       url: url,
-      jsonCallback: 'jsonp',
-      data: { q: city, APPID:'4846f8ae0e168b5a24906e0c47ff92b5' },
+      jsonCallback: 'fetchData',
+      data: { q: city, APPID: apiKey },
       cache: true,
       success: function (data) {
           data.city = city;
@@ -15,23 +16,56 @@ $(function () {
           var widget = showCity(data);
           $('.weatherData').html(widget);
       }
+    }).fail(function(error) {
+      console.error(error);
+      alert('Error sending request');
     });
   }
 
-  var cities = ['London', 'Berlin', 'New York', 'Tokyo', 'New Dehli', 'Moskau', 'Sydney'];
+//Randomize cities
+  var cities = ['London', 'Berlin', 'New York', 'Tokyo', 'New Dehli', 'Moskau', 'Sydney', 'Oakland', 'Los Angeles', 'Boston', 'Rome'];
 
-  $('.submit').click(function() {
+  $('.random').click(function() {
     var rCities = cities[Math.floor(Math.random()*cities.length)];
-		console.log(rCities);
+		console.log('rCities', rCities);
     getWeather(rCities, function(returnedData) {
-    	console.log(returnedData)
+    	console.log('returnedData', returnedData)
+      showCity();
     })
-
   });
 
-  function showCity() {
+  function showCity(data) {
     return "<h3>Weather: "+ data.weather[0].main  +"</h3>" +
            "<h3>Weather: "+ data.weather[0].description  +"</h3>";
   }
+
+  var prepareData = function(units) {
+    var cityName = $('.cityName').val()
+    if (cityName && cityName != '') {
+      cityName = cityName.trim()
+      getWeather()
+    } else {
+      alert ('Please enter a city name');
+    }
+  }
+
+  $('.btn-metric').click(function() {
+    prepareData('metric');
+  })
+
+
+  function fetchData (forecast) {
+    console.log('forecast', forecast);
+    var html = '',
+    cityName = forecast.city.name,
+    country = forecast.city.country //sys.country
+
+      html += '<h3> WEather Forecast for ' + cityName + ', ' + country + '</h3>'
+      forecast.list.forEach(function(forecastEntry, index, list) {
+        html += '<p>' + forecastEntry.dt_txt + ': ' + forecastEntry.main.temp + '</p>';
+      })
+  }
+
+
 
 });
